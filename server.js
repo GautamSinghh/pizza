d8e0f84c6a3a01b2f5a2954222b92d785f99dc42
GautamSinghh <gautamsingh@vocvision.com>
2021-05-22 03:26:41 +0530

    add cart page, auth

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const expressLayout = require('express-ejs-layouts')
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res) =>{
-res.render('home')
-})
 
 // asserts
 
@@ -21,6 +18,23 @@ app.use(expressLayout)
 app.set('views', path.join(__dirname,'/resources/views'))
 app.set('view engine', 'ejs')
 
+
+app.get('/',(req,res) =>{
+    res.render('home')
+    })
+    
+    app.get('/cart',(req,res) =>{
+        res.render('customers/cart')
+        })
+
+        app.get('/login',(req,res) =>{
+            res.render('auth/login')
+            })
+    
+            app.get('/register',(req,res) =>{
+                res.render('auth/register')
+                })
+        
 
 app.listen(PORT, () =>{
     console.log("Listening on port 3000")
